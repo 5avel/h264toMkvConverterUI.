2d6ce2819209bb7c5e3ad0b5363696c2b88ee4f5
5avel <5avel@5AVEL-PC>
2017-03-13 09:08:04 +0200

    Добавьте файлы проекта.

---
+++ FFMPEGConverterUI/Converter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FFMPEGConverterUI
+{
+    class Converter : IDisposable
+    {
+        public string InFolderPath { get; set; }
+        public string OutFolderPath { get; set; }
+
+        private  Process m_ffmpegProcess = null;
+
+        private  StreamReader m_streamReader = null;
+
+        private  CultureInfo m_culture = new CultureInfo(0x0409);
+
+        public Converter()
+        {
+            m_ffmpegProcess = new Process();
+            m_ffmpegProcess.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\lib\\ffmpeg.exe"; // путь до ffmpeg.exe
+            m_ffmpegProcess.StartInfo.UseShellExecute = false;
+            m_ffmpegProcess.StartInfo.RedirectStandardError = true;
+            m_ffmpegProcess.StartInfo.RedirectStandardOutput = true;
+            m_ffmpegProcess.StartInfo.CreateNoWindow = true; // скрываем процесс в панели
+        }
+
+
+        public  bool StartConvert(string fileName, bool repair = false)
+        {
+            if (repair)
+            {
+                m_ffmpegProcess.StartInfo.Arguments = @" -f h264 -r 12 -y -i " + InFolderPath + "\\" + fileName
+                + @" -vcodec libx264 -preset ultrafast -acodec copy "+OutFolderPath+"\\" + fileName + ".mkv"; // параметры конвертации
+            }
+            else
+            {
+                m_ffmpegProcess.StartInfo.Arguments = @" -r 12 -y -i " + InFolderPath + "\\" + fileName
+                + @" -vcodec copy -acodec copy " + OutFolderPath + "\\" + fileName + ".mkv"; // параметры конвертации
+            }
+
+            try
+            {
+                m_ffmpegProcess.Start();
+                m_streamReader = m_ffmpegProcess.StandardError;
+
+                string outString = null;
+                bool ret = false;
+                while (!m_streamReader.EndOfStream)
+                {
+                    outString = m_streamReader.ReadLine();
+
+                    if (outString.Contains("Conversion failed!")) return false;
+
+                    if (outString.Contains("headers:0kB"))
+                    {
+                        ret = true;
+                    }
+                }
+                return ret;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                m_ffmpegProcess.WaitForExit();
+                m_ffmpegProcess.Close();
+            }
+        }
+
+        public void Dispose()
+        {
+            m_ffmpegProcess.Dispose();
+        }
+    }
+}
---
+++ FFMPEGConverterUI/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+// Install-Package Windows7APICodePack-Shell
+
+namespace FFMPEGConverterUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] fileArray;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inBtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    tbInFolder.Text = dlg.SelectedPath;
+                }
+            }
+
+            fileArray = Directory.GetFiles(tbInFolder.Text, "*.h264", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+                fileArray[i] = Path.GetFileName(fileArray[i]);
+            }
+
+            pbProgres.Maximum = fileArray.Length;
+
+            tbConsole.Text += "В папке:'" + tbInFolder.Text + "' - " + fileArray.Count() + " файлов *.h264. \n";
+        }
+
+        private void outBtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    tbOutFolder.Text = dlg.SelectedPath;
+                }
+            }
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (fileArray == null && fileArray.Count() < 0)
+            {
+                System.Windows.MessageBox.Show("В папке '" + tbInFolder.Text + "' нет файлов *h264!");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(tbOutFolder.Text))
+            {
+                System.Windows.MessageBox.Show("Указана не существующая папка!");
+                return;
+            }
+
+            string inFolder = tbInFolder.Text;
+            string outFolder = tbOutFolder.Text;
+            bool isRepair = cbRepair.IsChecked.Value;
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                using (var h264toMKV = new Converter() { InFolderPath = inFolder, OutFolderPath = outFolder })
+                {
+
+                    DateTime allStartDT = DateTime.Now;
+                    int counter = 0;
+                    foreach (var item in fileArray)
+                    {
+                        counter++;
+                        DateTime itemStartDT = DateTime.Now;
+                        if (h264toMKV.StartConvert(item))
+                        {
+                        tbConsole.Dispatcher.BeginInvoke((Action)(() =>
+                        {
+                            tbConsole.Text += item + " за " + (DateTime.Now - itemStartDT).ToString() + " \n";
+                        }));
+                        
+                        }
+                        else
+                        {
+                            if (isRepair)
+                            {
+                                h264toMKV.StartConvert(item, true);
+                                tbConsole.Dispatcher.BeginInvoke((Action)(() =>
+                                {
+                                    tbConsole.Text += item + " Был поврежден, исправлен за " + (DateTime.Now - itemStartDT).ToString() + " \n";
+                                }));
+                            }
+                            else
+                            {
+                                tbConsole.Dispatcher.BeginInvoke((Action)(() =>
+                                {
+                                    tbConsole.Text += item + " за " + (DateTime.Now - itemStartDT).ToString() + " ----- ПОВРЕЖДЕН!!! \n";
+                                }));
+                            }
+                        }
+
+                        pbProgres.Dispatcher.BeginInvoke((Action)(() =>
+                        {
+                            pbProgres.Value = counter;
+                        }));
+                    }
+
+                tbConsole.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    tbConsole.Text += "Финиш за " + (DateTime.Now - allStartDT).ToString() + " \n";
+                    pbProgres.Value = 0;
+                }));  
+            }
+          });
+
+        }
+    }
+}
